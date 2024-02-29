11bac20f7848450882acd733904089312061266a
Dami <damisco005@gmail.com>
2024-02-29 16:08:09 +0100

    Hmmm.. flask with python

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 # Using Puppet, install flask from pip3
 
+package { 'werkzeug':
+  ensure   => '0.16.1',
+  provider => 'pip3',
+  require  => Package['flask'],
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  before   => Package['werkzeug']
 }
