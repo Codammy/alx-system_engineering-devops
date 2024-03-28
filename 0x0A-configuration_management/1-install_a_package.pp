05de8a369f377c252345aed48933da7aec5bc678
Dami <damisco005@gmail.com>
2024-03-28 17:02:08 +0100

    1-install_a_package.pp 0-create_a_file.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,12 @@
 
 package {'flask':
   ensure   => '2.1.0',
-  provider => pip3
+  provider => pip3,
+  before   => Package['werkzeug']
+}
+
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => pip3,
+  require  => Package['flask']
 }
