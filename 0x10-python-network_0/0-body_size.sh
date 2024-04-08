444aa9b76810bf52e34f0a29d773c8abbaeb1ded
Dami <damisco005@gmail.com>
2024-04-08 14:00:25 +0100

    0-body_size.sh README.md

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Bash script that takes in a URL,
+# sends a request to that URL,
+# and displays the size of the body of the response
+
+curl -sI $1 | grep "Content-Length" | cut -c17- -;
