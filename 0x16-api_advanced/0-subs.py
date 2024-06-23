3e44a3ad8010c5f65044c77143d28ce269a23bec
Codam <damisco005@gmail.com>
2024-06-23 15:25:50 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,9 @@ import sys
 
 def number_of_subscribers(subreddit):
     """queries number of subscribers for a given subreddit"""
+
+    if type(subreddit) is not str:
+        return 0
     url = f"https://api.reddit.com/r/{subreddit}/about"
 
     res = requests.get(url, headers={'User-Agent': 'Laptop'})
