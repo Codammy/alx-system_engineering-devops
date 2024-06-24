c7f2701b2dcb4b6460d6f433cd3275ae06fc1022
Dami <damisco005@gmail.com>
2024-06-24 05:13:20 +0100

    2-recurse.py __pycache__

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,21 +4,21 @@ recursive function that queries the Reddit API and returns a
 list containing the titles of all hot articles for a given subreddit.
 """
 import requests
-import time
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, after=None, count=0, hot_list=[]):
     """recursive function that makes requeet to reddit api"""
     url = f'https://api.reddit.com/r/{subreddit}'
-    res = requests.get(url, params=hot_list)
-    after = [res.json().get('data').get('after')]
-    print(after)
+    res = requests.get(url, params={'after': after},
+                       headers={'User-Agent': 'github.com/codammy'})
     if res.ok and not res.is_redirect:
-        data = res.json().get('data').get('children')
+        count += 1
+        data_all = res.json()
+        after = data_all.get('data').get('after')
+        data = data_all.get('data').get('children')
         for d in data:
-            print(d['data']['title'])
-        time.sleep(2)
-        print()
-        recurse(subreddit, after)
-
-recurse('programming')
+            hot_list.append(d['data']['title'])
+        if after is None:
+            return hot_list
+        return recurse(subreddit, after, count, hot_list)
+    return None
