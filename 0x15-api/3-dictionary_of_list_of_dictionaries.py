dc0edb50bb3afc9085a7e08f8a004b14540b7a1a
Dami <damisco005@gmail.com>
2024-07-30 08:40:41 +0100

    3-dictionary_of_list_of_dictionaries.py 2-export_to_JSON.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,9 +4,9 @@ Python script that, using this REST API,
 for a given employee ID,
 returns information about his/her TODO list progress.
 """
+import json
 import requests
 import sys
-import json
 url = "https://jsonplaceholder.typicode.com/"
 
 
