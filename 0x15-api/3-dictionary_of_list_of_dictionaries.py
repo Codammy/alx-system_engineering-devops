9ed668f9afeca18cad9c1a2ada9a6c5253e64510
Dami <damisco005@gmail.com>
2024-07-31 08:56:48 +0100

    todo_all_employees.json 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -16,13 +16,19 @@ def fetch_user_tasks(userid, username):
     tasks = list(map(lambda obj: {"username": username,
                                   "task": obj["title"],
                                   "completed": obj["completed"]}, response))
-    with open("todo_all_employees.json", "a") as fp:
-        json.dump({userid: tasks}, fp)
+    return tasks
 
 
 if __name__ == "__main__":
     response = requests.get(url + "users")
     if response.ok:
         user_data = response.json()
+        all_user_tasks = dict()
+
         for info in user_data:
-            fetch_user_tasks(info.get('id'), info.get('username'))
+            user_id = info.get('id')
+            user_tasks = fetch_user_tasks(user_id, info.get('username'))
+            all_user_tasks[user_id] = user_tasks
+
+        with open("todo_all_employees.json", "w") as fp:
+            json.dump(all_user_tasks, fp)
